af0351f7cd24a4579cf17cb39cbc0771baf57efd
Sharmili0127 <ksharmili27@gmail.com>
2026-07-25 21:24:37 +0530

    Time: 2 ms (17.67%) | Memory: 42.3 MB (19.60%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+
+        int i = 1;
+
+        while ((long) i * i <= num) {
+            if ((long) i * i == num) {
+                return true;
+            }
+            i++;
+        }
+
+        return false;
+    }
+}
